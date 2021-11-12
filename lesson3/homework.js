08c905a26578d0a42f68cef2cc85d2049374225c
Filip Dabkowski <filip.dabkowski@gmail.com>
2021-11-12 21:53:44 +0100

    add homework3

---
+++ lesson3/homework.js
@@ -0,0 +1,24 @@
+
+const questions = [
+    "Czy zwierzęta przeprowadzają fotosyntezę?",
+    "Czy Homo Sapiens to jedyny gatunek ludzki w historii?",
+    "Czy żarówki halogenowe wytwarzają dużo ciepła?",
+    "Czy Szwajcaria należy do Unii Europejskiej?",
+    "Czy MacBook'i są warte swojej ceny?",
+    "Czy kobiety powinny mieć prawo do aborcji?",
+    "Czy uważasz czy geje i lesbijki powinny mieć dzieci?",
+    "Czy nauczyciel religi powinien kazać nam pisać rozprawki?"
+]
+const answers = [true, false, true, false, true, true, true, false]
+
+// 1. napisz funkcje ktora zadaje urztkownikowi pyatnie po koleji zlisty `questions`.
+// tip: confirm
+
+// 2. zapisz odpowiedz urzytkownika w zmiennej i porownaj ja z zapisana w liscie `answers`
+// tip: numer pytania i odpowiedzi sa takie same
+
+// 3. pokaz urzytkownikowi czy jego odpowiedz jest taka sama jak zapisana w odpowiedziach
+// tip: alert
+
+
+// powinna byc funkcja do puszczania wszytkiego i jedna do wyswitlania pokazywania odpowiedzi
